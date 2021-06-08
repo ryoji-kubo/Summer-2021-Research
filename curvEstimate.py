b7ccd8c5723070c6f66936ebbcdffbb2c735e933
Ryoji Kubo <56710428+ryoji-kubo@users.noreply.github.com>
2021-06-09 02:59:39 +0900

    added reference from original code on implementation of Distance Matrix D

--- curvEstimate.py
+++ curvEstimate.py
@@ -95,6 +95,42 @@ def estimate(dataset='data/edges/smalltree.edges', n_samples=100000):
 
 
 
+"""
+For reference: how HazyResearch implements distance matrix from the helper functions
+https://github.com/HazyResearch/hyperbolics/blob/dedf0511f715a02152ba9287439a75c33aca2ef4/pytorch/graph_helpers.py#L12
+"""
+
+def djikstra_wrapper( _x ):
+    (mat, x) = _x
+    return csg.dijkstra(mat, indices=x, unweighted=False, directed=False)
+
+def build_distance(G, scale, num_workers=None):
+    n = G.order()
+    p = Pool() if num_workers is None else Pool(num_workers)
+    
+    #adj_mat_original = nx.to_scipy_sparse_matrix(G)
+    adj_mat_original = nx.to_scipy_sparse_matrix(G, nodelist=list(range(G.order())))
+
+    # Simple chunking
+    nChunks     = 128 if num_workers is not None and num_workers > 1 else n
+    if n > nChunks:
+        chunk_size  = n//nChunks
+        extra_chunk_size = (n - (n//nChunks)*nChunks)
+            
+
+        chunks     = [ list(range(k*chunk_size, (k+1)*chunk_size)) for k in range(nChunks)]
+        if extra_chunk_size >0: chunks.append(list(range(n-extra_chunk_size, n)))
+        Hs = p.map(djikstra_wrapper, [(adj_mat_original, chunk) for chunk in chunks])
+        H  = np.concatenate(Hs,0)
+        logging.info(f"\tFinal Matrix {H.shape}")
+    else:
+        H = djikstra_wrapper( (adj_mat_original, list(range(n))) )
+        
+    H *= scale
+    return H
+
+
+
 
 
 
